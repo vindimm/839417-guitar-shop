991ada14fd99e097a055193d3c589878cbe6e24d
Dimahtml <bomabo@mail.ru>
2022-06-21 17:43:44 +0500

    форма поиска теперь закрывается при событии onBlur

--- src/components/common/header/search-form/search-form.tsx
+++ src/components/common/header/search-form/search-form.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, ChangeEvent } from 'react';
+import { useState, useEffect, ChangeEvent, FocusEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useAppDispatch, useAppSelector } from '../../../../hooks';
@@ -22,6 +22,18 @@ function SearchForm (): JSX.Element {
     dispatch(resetGuitarsBySearch());
   };
 
+  const handleBlurComponent = (evt: FocusEvent<HTMLDivElement>) => {
+    if (!evt.currentTarget.contains(evt.relatedTarget)) {
+      setSearchValue('');
+      dispatch(resetGuitarsBySearch());
+    }
+  };
+
+  const handleClickLink = () => {
+    setSearchValue('');
+    dispatch(resetGuitarsBySearch());
+  };
+
   useEffect(() => {
     if (searchValue) {
       dispatch(fetchGuitarsBySearchAction(searchValue));
@@ -29,7 +41,7 @@ function SearchForm (): JSX.Element {
   }, [dispatch, searchValue]);
 
   return (
-    <div className="form-search">
+    <div className="form-search" onBlur={handleBlurComponent}>
       <form className="form-search__form" id="form-search">
         <button className="form-search__submit" type="submit">
           <svg className="form-search__icon" width="14" height="15" aria-hidden="true">
@@ -51,13 +63,13 @@ function SearchForm (): JSX.Element {
         {guitars.map((guitar) => (
           <li
             className="form-search__select-item"
-            tabIndex={0}
             key={guitar.id}
           >
             <Link
               className="form-search__select-item"
               to={AppRoute.GuitarPage.replace(':id', String(guitar.id))}
               style={{display: 'inline-block', width: '100%', padding: '0'}}
+              onClick={handleClickLink}
             >
               {guitar.name}
             </Link>
